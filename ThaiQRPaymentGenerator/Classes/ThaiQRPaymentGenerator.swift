cf627a503276e8a66816a2fe380b8a0ab5694b85
LostSeaWay <lostseaway@me.com>
2019-08-16 02:47:02 +0700

    fix from pod lint

--- ThaiQRPaymentGenerator/Classes/ThaiQRPaymentGenerator.swift
+++ ThaiQRPaymentGenerator/Classes/ThaiQRPaymentGenerator.swift
@@ -40,7 +40,7 @@ public class ThaiQRPaymentGenerator {
             filter.setValue(data, forKey: "inputMessage")
             let transform = CGAffineTransform(scaleX: 3, y: 3)
             
-            if let output = filter.outputImage?.transformed(by: transform) {
+            if let output = filter.outputImage?.applying(transform) {
                 return UIImage(ciImage: output)
             }
         }
